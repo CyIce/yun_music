30bbef2439519142519f34d8c94e16e84398a199
Administrator <1207201395@qq.com>
2018-06-30 16:33:12 +0800

    编写爬虫主程序。

---
+++ yun_music/spiders/music_spider.py
@@ -0,0 +1,29 @@
+import scrapy
+
+from yun_music.items import SingerItem,SongItem
+
+class yun_music_spider(scrapy.Spider):
+
+    # 爬虫名称
+    name="music_spider"
+    # 爬虫的允许域范围
+    allowed_domains=[]
+    # 爬虫其实url
+    start_urls=[]
+    # 基础url，用于拼接url
+    base_url=""
+    # 爬虫已爬去的次数
+    crawl_times=0
+
+
+    def parse(self,response):
+
+        pass
+
+    def parse_singer(self,response):
+
+        pass
+
+    def parse_song(self,response):
+
+        pass
