757d167becefc361990d52a2453a80c2a1789d92
cyice <1207201395@qq.com>
2018-07-01 23:46:03 +0800

    使用Selenium模块，添加爬虫中间件跳入iframe。

--- yun_music/middlewares.py
+++ yun_music/middlewares.py
@@ -6,6 +6,25 @@
 # https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 
 from scrapy import signals
+from selenium import webdriver
+import selenium.webdriver.support.ui as ui
+from scrapy.http import HtmlResponse
+
+
+class MyMusicDownloaderMiddleware(object):
+
+    def __init__(self):
+        self.driver = webdriver.Chrome()  # 指定使用的浏览器
+
+    def process_request(self, request, spider):
+        if spider.name == "music_spider":
+            self.driver.get(request.url)
+            self.driver.switch_to.frame("contentFrame")
+            body = self.driver.page_source
+            return HtmlResponse(self.driver.current_url, body=body, encoding='utf-8', request=request)
+        else:
+            return
+
 
 
 class YunMusicSpiderMiddleware(object):
--- yun_music/spiders/music_spider.py
+++ yun_music/spiders/music_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from bs4 import BeautifulSoup
 
 from yun_music.items import SingerItem,SongItem
 
@@ -7,18 +8,26 @@ class yun_music_spider(scrapy.Spider):
     # 爬虫名称
     name="music_spider"
     # 爬虫的允许域范围
-    allowed_domains=[]
-    # 爬虫其实url
-    start_urls=[]
+    allowed_domains=['music.163.com']
+    # 爬虫起始url
+    start_urls=['https://music.163.com/#/discover/artist/cat?id=1001&initial=65']
     # 基础url，用于拼接url
-    base_url=""
+    base_url="https://music.163.com/"
     # 爬虫已爬去的次数
     crawl_times=0
 
 
     def parse(self,response):
 
-        pass
+        soup=BeautifulSoup(response.body,'html.parser',from_encoding='utf-8')
+
+        print("==================================================")
+        print(soup)
+        print("==================================================")
+
+
+        #yield scrapy.Request("https://music.163.com/#/discover/artist/cat?id=1001&initial=66", callback=self.parse)
+
 
     def parse_singer(self,response):
 
